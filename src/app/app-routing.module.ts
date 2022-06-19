d277e59746390aeea475d3ac0971e75f8c494ca2
willytec <alown666@gmail.com>
2022-06-19 05:43:42 +0200

    Some fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListComponent } from './list/list.component';
 
-const routes: Routes = [{ path: 'list', component: ListComponent }];
+const routes: Routes = [
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
+  { path: 'list', component: ListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
