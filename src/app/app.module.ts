efc070b5a9eed3f9b023c1c836ffbb3897e281ea
willytec <alown666@gmail.com>
2022-06-27 07:55:59 +0200

    Some tweak to the date and rate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,7 +12,9 @@ import { SideBarComponent } from './side-bar/side-bar.component';
 import { ListComponent } from './list/list.component';
 import { PaginationComponent } from './pagination/pagination.component';
 import { MinicardComponent } from './minicard/minicard.component';
-
+import localeFr from '@angular/common/locales/fr';
+import { RatingComponent } from './rating/rating.component';
+registerLocaleData(localeFr, 'fr');
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,6 +25,7 @@ import { MinicardComponent } from './minicard/minicard.component';
     ListComponent,
     PaginationComponent,
     MinicardComponent,
+    RatingComponent,
   ],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
